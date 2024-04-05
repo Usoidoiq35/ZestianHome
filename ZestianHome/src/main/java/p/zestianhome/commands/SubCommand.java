ac5a8bcb9cfc906d56906a08ef6f24aa5171e15a
Spinnin <123275059+Spinnin34@users.noreply.github.com>
2024-04-05 16:05:47 +0200

    Delete ZestianHome directory

--- ZestianHome/src/main/java/p/zestianhome/commands/SubCommand.java
+++
@@ -1,9 +0,0 @@
-package p.zestianhome.commands;
-
-import org.bukkit.command.CommandSender;
-import java.io.IOException;
-
-public interface SubCommand {
-
-    void execute(CommandSender sender, String[] args) throws IOException;
-}
